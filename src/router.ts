399d7b5b594873bff068f203ff599af123814e32
Pedro Barros <pedrobars7a@gmail.com>
2022-11-15 19:30:52 -0300

    create router file

---
+++ src/router.ts
@@ -0,0 +1,48 @@
+import { Router } from 'express';
+
+export const router = Router();
+
+// List categories
+router.get('/categories', (request, response) => {
+  response.send('OK');
+});
+
+// Create category
+router.post('/categories', (request, response) => {
+  response.send('OK');
+});
+
+// List products
+router.get('/products', (request, response) => {
+  response.send('OK');
+});
+
+// Create product
+router.post('/products', (request, response) => {
+  response.send('OK');
+});
+
+// Get products by category
+router.get('/categories/:categoryId/products', (request, response) => {
+  response.send('OK');
+});
+
+// List orders
+router.get('/orders', (request, response) => {
+  response.send('OK');
+});
+
+// Create order
+router.post('/orders', (request, response) => {
+  response.send('OK');
+});
+
+// Change order status
+router.patch('/orders/:orderId', (request, response) => {
+  response.send('OK');
+});
+
+// Delete/cancel order
+router.delete('/orders/:orderId', (request, response) => {
+  response.send('OK');
+});

355b2ccc6f556b667d1ab73253f23ea8731c1e2d
Pedro Barros <pedrobars7a@gmail.com>
2022-11-16 06:47:18 -0300

    improve createCategory

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -3,12 +3,16 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function createCategory(request: Request, response: Response) {
-  const { icon, name } = request.body;
+  try {
+    const { icon, name } = request.body;
 
-  const category = await Category.create({
-    icon,
-    name,
-  });
+    const category = await Category.create({
+      icon,
+      name,
+    });
 
-  return response.json(category);
+    return response.status(201).json(category);
+  } catch {
+    response.sendStatus(500);
+  }
 }
